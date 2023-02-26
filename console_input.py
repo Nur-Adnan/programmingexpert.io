80ea988080f33019e331f9b615cfad7d0c933d39
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2023-02-26 08:44:53 +0600

    input_console_complete

---
+++ console_input.py
@@ -0,0 +1,19 @@
+number = input ("Number: ");
+
+# number = input ("Number: ", "Age: ");
+# input expected at most 1 argument but got 2
+
+print("Hello, you age is", number);
+
+# whatever you typing in this input function, all the things will be string
+
+name = input("Name: ");
+print(type(name));
+
+input("Press enter to begin");
+
+
+name = input ("Enter your name: ");
+age = input("Hello, " + name + "what is your age: ");
+
+print("Hello," + name + "you are in" + age + "years old !");
