1baa015d52bab50a43bab22c59d62e617d670834
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2023-02-26 08:28:37 +0600

    variables&printing

---
+++ variables&priting.py
@@ -0,0 +1,22 @@
+# print("Hello\n",5, True); 
+# #end=" " == \n
+print("Hello",5, True, end= " | ")
+print("Adnan");
+
+x = 3;
+y = "hello";
+num = y;
+print(x);
+print(num,y);
+
+
+a = 3;
+print(a);
+b = "hello";
+print(b);
+num = b;
+print(num);
+b = a;
+print(b);
+a = 4;
+print(a);
